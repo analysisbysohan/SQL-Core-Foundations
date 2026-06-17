21c0cd60721ce36a6630702af78cf84150a7c79c
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-06-17 15:55:55 +0530

    Create 02_multi_condition_filtering.sql

---
+++ SQL/02_multi_condition_filtering.sql
@@ -0,0 +1,7 @@
+-- Challenge 1: Finding Action movies with high earnings using AND
+SELECT * FROM Movies
+WHERE Genre = 'Action' AND Box_Office_Millions > 500;
+
+-- Challenge 2: Finding specific family or mature rating types using OR
+SELECT * FROM Movies
+WHERE Rating_Type = 'PG' OR Rating_Type = 'R';
