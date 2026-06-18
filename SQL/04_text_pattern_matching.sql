35742255498e4764da8be5f457ebc9f3ad9cca2b
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-06-18 20:34:59 +0530

    Create 04_text_pattern_matching.sql

---
+++ SQL/04_text_pattern_matching.sql
@@ -0,0 +1,7 @@
+-- Challenge 1: Finding a word anywhere in the title (Contains)
+SELECT * FROM MOVIES 
+WHERE Title LIKE '%KNIGHT%';
+
+-- Challenge 2: Finding a word at the very beginning (Starts With)
+SELECT * FROM MOVIES 
+WHERE TITLE LIKE 'STAR%';
