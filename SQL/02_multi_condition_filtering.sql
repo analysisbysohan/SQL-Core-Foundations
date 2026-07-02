b76f9600ac7f23be19be327c66aede604887cb2f
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-07-02 21:39:43 +0530

    Update 02_multi_condition_filtering.sql

--- SQL/02_multi_condition_filtering.sql
+++ SQL/02_multi_condition_filtering.sql
@@ -5,4 +5,3 @@ WHERE Genre = 'Action' AND Box_Office_Millions > 500;
 -- Challenge 2: Finding specific family or mature rating types using OR
 SELECT * FROM Movies
 WHERE Rating_Type = 'PG' OR Rating_Type = 'R';
-
