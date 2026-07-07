19dfe945081e0bc32b8d1339bcf80d146e06d377
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-07-07 16:04:36 +0530

    Update 07_Creating_Tables.sql

--- SQL/07_Creating_Tables.sql
+++ SQL/07_Creating_Tables.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,   -- Made this the unique identifier
+    first_name VARCHAR(50) NOT NULL, -- Added length and forced it to not be blank
+    hire_date DATE,
+    salary DECIMAL(10, 2)          -- Best practice for money (10 digits total, 2 for decimals)
+);
