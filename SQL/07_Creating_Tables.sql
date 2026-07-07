c69b86d7abeb38d4789062cffcac524ebd50ee7c
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-07-07 16:09:12 +0530

    Update 07_Creating_Tables.sql

--- SQL/07_Creating_Tables.sql
+++ SQL/07_Creating_Tables.sql
@@ -4,3 +4,8 @@ CREATE TABLE employees (
     hire_date DATE,
     salary DECIMAL(10, 2)          -- Best practice for money (10 digits total, 2 for decimals)
 );
+
+CREATE TABLE students (
+    student_id INT,
+    student_name VARCHAR
+);
