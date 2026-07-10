c2ae694c91b7d4f73026ff55265023ac1c26eda9
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-07-10 12:01:55 +0530

    Update 08_Filtering_Data.sql

--- SQL/08_Filtering_Data.sql
+++ SQL/08_Filtering_Data.sql
@@ -1,3 +1,7 @@
 -- Filtering data using the WHERE clause
 SELECT * FROM students
 WHERE student_name = 'RAHUL';
+
+-- Filtering numbers using comparison operators
+SELECT * FROM products
+WHERE price <= 150;
