dd5e473015cfdecbf8263f327d03f15f849609bc
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-07-10 12:15:39 +0530

    Create 09_Sorting_Data.sql

---
+++ SQL/09_Sorting_Data.sql
@@ -0,0 +1,7 @@
+-- Sorting data in ascending order (Cheapest first)
+SELECT * FROM products 
+ORDER BY price;
+
+-- Sorting data in descending order (Most expensive first)
+SELECT * FROM products 
+ORDER BY price DESC;
