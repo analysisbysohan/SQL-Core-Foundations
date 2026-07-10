2595d916f7df221453039a22d1fb69c79201e1ba
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-07-10 12:08:17 +0530

    Update 08_Filtering_Data.sql

--- SQL/08_Filtering_Data.sql
+++ SQL/08_Filtering_Data.sql
@@ -5,3 +5,8 @@ WHERE student_name = 'RAHUL';
 -- Filtering numbers using comparison operators
 SELECT * FROM products
 WHERE price <= 150;
+
+-- Filtering with multiple conditions using AND
+SELECT * FROM employees
+WHERE department = 'IT' 
+  AND salary > 60000;
