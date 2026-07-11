3c4dde9295092feda55ad0d25dcca9f8a57e2950
analysisbysohan <289674497+analysisbysohan@users.noreply.github.com>
2026-07-11 12:43:28 +0530

    create table

--- SQL/10_End_To_End_Practice.sql
+++ SQL/10_End_To_End_Practice.sql
@@ -1,1 +1,8 @@
-
+-- Step 1: Create the Table structure
+CREATE TABLE projects (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    team VARCHAR(30),
+    budget DECIMAL(10, 2),
+    launch_date DATE
+);
